364436c9bd53a08fba57d4dd16e1ded0b1f2ca27
Phil <phil@edojo.org>
2015-05-18 20:30:52 +0200

    Initial commit.
    * Add SpringBoot backend with dependencies.

---
+++ backend/src/main/java/org/edojo/server/EdojoServerApplication.java
@@ -0,0 +1,12 @@
+package org.edojo.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdojoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdojoServerApplication.class, args);
+    }
+}
