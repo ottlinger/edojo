95ab8344660bc0099463a1ad1646a42cbeb2cac9
Hugo Hirsch <github@hugo-hirsch.de>
2016-04-26 07:52:12 +0200

    Add test and fix travis/codecov issues.

--- backend/src/test/java/org/edojo/BackendApplicationTests.java
+++ backend/src/test/java/org/edojo/BackendApplicationTests.java
@@ -1,5 +1,7 @@
 package org.edojo;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +13,7 @@ public class BackendApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		assertTrue(true);
 	}
 
 }
