44569709e8f895a88f1f9ef6545dad43c469281a
Hugo Hirsch <github@hugo-hirsch.de>
2020-11-02 13:08:17 +0100

    Fix participant test

--- registrar/src/test/groovy/registrar/ParticipantControllerSpec.groovy
+++ registrar/src/test/groovy/registrar/ParticipantControllerSpec.groovy
@@ -3,16 +3,17 @@ package registrar
 import grails.testing.gorm.DomainUnitTest
 import grails.testing.web.controllers.ControllerUnitTest
 import grails.validation.ValidationException
-import spock.lang.*
+import spock.lang.Specification
 
 class ParticipantControllerSpec extends Specification implements ControllerUnitTest<ParticipantController>, DomainUnitTest<Participant> {
 
     def populateValidParams(params) {
         assert params != null
 
-        // TODO: Populate valid properties like...
-        //params["name"] = 'someValidName'
-        assert false, "TODO: Provide a populateValidParams() implementation for this generated test suite"
+        params["name"] = 'Doe the ' + Math.random()
+        params["firstName"] = 'John'
+        params["telephoneContact"] = '+440123456789'
+        params["emailContact"] = 'john.doe@example.org'
     }
 
     void "Test the index action returns the correct model"() {
@@ -22,29 +23,29 @@ class ParticipantControllerSpec extends Specification implements ControllerUnitT
             1 * count() >> 0
         }
 
-        when:"The index action is executed"
+        when: "The index action is executed"
         controller.index()
 
-        then:"The model is correct"
+        then: "The model is correct"
         !model.participantList
         model.participantCount == 0
     }
 
     void "Test the create action returns the correct model"() {
-        when:"The create action is executed"
+        when: "The create action is executed"
         controller.create()
 
-        then:"The model is correctly created"
-        model.participant!= null
+        then: "The model is correctly created"
+        model.participant != null
     }
 
     void "Test the save action with a null instance"() {
-        when:"Save is called for a domain instance that doesn't exist"
+        when: "Save is called for a domain instance that doesn't exist"
         request.contentType = FORM_CONTENT_TYPE
         request.method = 'POST'
         controller.save(null)
 
-        then:"A 404 error is returned"
+        then: "A 404 error is returned"
         response.redirectedUrl == '/participant/index'
         flash.message != null
     }
@@ -55,7 +56,7 @@ class ParticipantControllerSpec extends Specification implements ControllerUnitT
             1 * save(_ as Participant)
         }
 
-        when:"The save action is executed with a valid instance"
+        when: "The save action is executed with a valid instance"
         response.reset()
         request.contentType = FORM_CONTENT_TYPE
         request.method = 'POST'
@@ -65,7 +66,7 @@ class ParticipantControllerSpec extends Specification implements ControllerUnitT
 
         controller.save(participant)
 
-        then:"A redirect is issued to the show action"
+        then: "A redirect is issued to the show action"
         response.redirectedUrl == '/participant/show/1'
         controller.flash.message != null
     }
@@ -78,13 +79,13 @@ class ParticipantControllerSpec extends Specification implements ControllerUnitT
             }
         }
 
-        when:"The save action is executed with an invalid instance"
+        when: "The save action is executed with an invalid instance"
         request.contentType = FORM_CONTENT_TYPE
         request.method = 'POST'
         def participant = new Participant()
         controller.save(participant)
 
-        then:"The create view is rendered again with the correct model"
+        then: "The create view is rendered again with the correct model"
         model.participant != null
         view == 'create'
     }
@@ -95,10 +96,10 @@ class ParticipantControllerSpec extends Specification implements ControllerUnitT
             1 * get(null) >> null
         }
 
-        when:"The show action is executed with a null domain"
+        when: "The show action is executed with a null domain"
         controller.show(null)
 
-        then:"A 404 error is returned"
+        then: "A 404 error is returned"
         response.status == 404
     }
 
@@ -108,10 +109,10 @@ class ParticipantControllerSpec extends Specification implements ControllerUnitT
             1 * get(2) >> new Participant()
         }
 
-        when:"A domain instance is passed to the show action"
+        when: "A domain instance is passed to the show action"
         controller.show(2)
 
-        then:"A model is populated containing the domain instance"
+        then: "A model is populated containing the domain instance"
         model.participant instanceof Participant
     }
 
@@ -121,10 +122,10 @@ class ParticipantControllerSpec extends Specification implements ControllerUnitT
             1 * get(null) >> null
         }
 
-        when:"The show action is executed with a null domain"
+        when: "The show action is executed with a null domain"
         controller.edit(null)
 
-        then:"A 404 error is returned"
+        then: "A 404 error is returned"
         response.status == 404
     }
 
@@ -134,21 +135,21 @@ class ParticipantControllerSpec extends Specification implements ControllerUnitT
             1 * get(2) >> new Participant()
         }
 
-        when:"A domain instance is passed to the show action"
+        when: "A domain instance is passed to the show action"
         controller.edit(2)
 
-        then:"A model is populated containing the domain instance"
+        then: "A model is populated containing the domain instance"
         model.participant instanceof Participant
     }
 
 
     void "Test the update action with a null instance"() {
-        when:"Save is called for a domain instance that doesn't exist"
+        when: "Save is called for a domain instance that doesn't exist"
         request.contentType = FORM_CONTENT_TYPE
         request.method = 'PUT'
         controller.update(null)
 
-        then:"A 404 error is returned"
+        then: "A 404 error is returned"
         response.redirectedUrl == '/participant/index'
         flash.message != null
     }
@@ -159,7 +160,7 @@ class ParticipantControllerSpec extends Specification implements ControllerUnitT
             1 * save(_ as Participant)
         }
 
-        when:"The save action is executed with a valid instance"
+        when: "The save action is executed with a valid instance"
         response.reset()
         request.contentType = FORM_CONTENT_TYPE
         request.method = 'PUT'
@@ -169,7 +170,7 @@ class ParticipantControllerSpec extends Specification implements ControllerUnitT
 
         controller.update(participant)
 
-        then:"A redirect is issued to the show action"
+        then: "A redirect is issued to the show action"
         response.redirectedUrl == '/participant/show/1'
         controller.flash.message != null
     }
@@ -182,23 +183,23 @@ class ParticipantControllerSpec extends Specification implements ControllerUnitT
             }
         }
 
-        when:"The save action is executed with an invalid instance"
+        when: "The save action is executed with an invalid instance"
         request.contentType = FORM_CONTENT_TYPE
         request.method = 'PUT'
         controller.update(new Participant())
 
-        then:"The edit view is rendered again with the correct model"
+        then: "The edit view is rendered again with the correct model"
         model.participant != null
         view == 'edit'
     }
 
     void "Test the delete action with a null instance"() {
-        when:"The delete action is called for a null instance"
+        when: "The delete action is called for a null instance"
         request.contentType = FORM_CONTENT_TYPE
         request.method = 'DELETE'
         controller.delete(null)
 
-        then:"A 404 is returned"
+        then: "A 404 is returned"
         response.redirectedUrl == '/participant/index'
         flash.message != null
     }
@@ -209,12 +210,12 @@ class ParticipantControllerSpec extends Specification implements ControllerUnitT
             1 * delete(2)
         }
 
-        when:"The domain instance is passed to the delete action"
+        when: "The domain instance is passed to the delete action"
         request.contentType = FORM_CONTENT_TYPE
         request.method = 'DELETE'
         controller.delete(2)
 
-        then:"The user is redirected to index"
+        then: "The user is redirected to index"
         response.redirectedUrl == '/participant/index'
         flash.message != null
     }
