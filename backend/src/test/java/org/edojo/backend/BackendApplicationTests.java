50830fe121401fda004db8cacf9e1eb27c06843d
Hugo Hirsch <github@hugo-hirsch.de>
2023-05-08 09:19:39 +0200

    Use newer version

--- backend/src/test/java/org/edojo/backend/BackendApplicationTests.java
+++ backend/src/test/java/org/edojo/backend/BackendApplicationTests.java
@@ -1,16 +1,13 @@
 package org.edojo.backend;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class BackendApplicationTests {
+class BackendApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	void contextLoads() {
 	}
 
 }
