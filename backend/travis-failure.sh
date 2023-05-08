d62f54616b1223da9f7b291b94b962807f41b5ac
Hugo Hirsch <github@hugo-hirsch.de>
2023-05-08 09:15:52 +0200

    No more travis

--- backend/travis-failure.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Since we cannot reach the tests you may cat a problematic test here - run 'gradle test' locally to see its name."
-echo 
-echo "***** ALL TESTS *****"
-ls backend/build/test-results/*.xml
-echo
-#echo "***** A SPECIFIC ONE *****"
-# cat backend/build/test-results/TEST-de.aikiit.fotocollector.main.JsonOutputWriterTest.xml
